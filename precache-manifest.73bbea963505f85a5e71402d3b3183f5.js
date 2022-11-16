54dc4ef82e8a9817f74bc2eb52fb4e768988d1f1
pahalpriya <priyapahal5@gmail.com>
2022-11-16 18:39:42 +0530

    Updates

---
+++ precache-manifest.73bbea963505f85a5e71402d3b3183f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ca40939faab1d412e037821bfd4e12",
+    "url": "/pahalpriya/pro/index.html"
+  },
+  {
+    "revision": "3e6e39189bf30d3cb8dc",
+    "url": "/pahalpriya/pro/static/css/main.83158f21.chunk.css"
+  },
+  {
+    "revision": "023fe1b411c6f4863612",
+    "url": "/pahalpriya/pro/static/js/2.4fd0d418.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pahalpriya/pro/static/js/2.4fd0d418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e6e39189bf30d3cb8dc",
+    "url": "/pahalpriya/pro/static/js/main.7f89bdbd.chunk.js"
+  },
+  {
+    "revision": "447df8f4e4bb5a00b824",
+    "url": "/pahalpriya/pro/static/js/runtime-main.022677b6.js"
+  }
+]);
